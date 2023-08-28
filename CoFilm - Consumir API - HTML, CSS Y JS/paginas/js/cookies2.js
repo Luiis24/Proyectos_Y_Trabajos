37514ac77ecc37aa94f18a6e58243c6e46a4219a
Luis <134552997+Luiis24@users.noreply.github.com>
2023-08-28 11:32:04 -0500

    Delete CoFilm - Consumir API - HTML, CSS Y JS directory

--- CoFilm - Consumir API - HTML, CSS Y JS/paginas/js/cookies2.js
+++
@@ -1,9 +0,0 @@
-document.body.addEventListener("click", (e) => {
-  if (e.target.className == "open-details-serie") {
-    e.preventDefault();
-    let serieId = e.target.id;
-    console.log(serieId);
-    localStorage.setItem("serieId", serieId);
-    window.open("./infoPelicula2.html","_self");
-  }
-});
